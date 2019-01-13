fe23f10fbb3e925dba82895e1232d793e5e7ecf5
Manikanta Tankala <im.manikanta@gmail.com>
2019-01-13 21:40:33 +0800

    #1 webpack config sourced from https://gist.github.com/learncodeacademy/25092d8f1daf5e4a6fd3

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  mode: debug ? "development" : "production",
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/scripts.js",
+  output: {
+    path: __dirname + "/js",
+    filename: "scripts.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      sourcemap: false
+    }),
+  ],
+};

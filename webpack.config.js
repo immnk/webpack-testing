65664827e512073f94257f199193ce334f49e89c
Manikanta <im.manikanta@gmail.com>
2019-01-14 12:45:40 +0800

    Refactor to use dist and src with webpack (#8)
    * #2 create src and generate dist folder with webpack using html-webpack-plugin
    
    * remove notification emails on build to stop spamming
    
    * adding codeowners in git

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: debug ? "development" : "production",
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
+  entry: "./src/js/scripts.js",
   output: {
-    path: __dirname + "/js",
+    path: __dirname + "/dist",
     filename: "scripts.min.js"
   },
-  plugins: debug ? [] : [
+  plugins: debug ? [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       mangle: false,
       sourcemap: false
     }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
   ],
 };

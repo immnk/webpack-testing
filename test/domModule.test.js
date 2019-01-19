6eb86f82ec6a2ccef0b61b208a293a86e78bfda3
Manikanta Tankala <im.manikanta@gmail.com>
2019-01-19 15:48:41 +0800

    #23 - test nyc coverage

--- test/domModule.test.js
+++ test/domModule.test.js
@@ -1,9 +1,11 @@
 const chai = require("chai");
 const expect = chai.expect;
+const assert = chai.assert;
 
 describe('domModule', function () {
     describe('Module#addMessageToDom', function () {
         before(function () {
+            this.timeout(0);
             const fs = require('fs');
             let htmlString = fs.readFileSync("./src/index.html").toString();
             this.jsdom = require("jsdom-global")(htmlString);
@@ -18,11 +20,11 @@ describe('domModule', function () {
             expect(this.domModule).to.be.a("object");
         });
 
-        // it("tests if dom can be created", function () {
-        //   let testMessage = "create test p element";
-        //   this.domModule.addMessageToDom("p", testMessage, "testClass");
-        //   let testElement = document.getElementsByClassName("testClass")[0].innerHTML;
-        //   assert.equal(testElement, testMessage);
-        // });
+        it("tests if dom can be created", function () {
+            let testMessage = "create test p element";
+            this.domModule.addMessageToDom("p", testMessage, "testClass");
+            let testElement = document.getElementsByClassName("testClass")[0].innerHTML;
+            assert.equal(testElement, testMessage);
+        });
     });
 });

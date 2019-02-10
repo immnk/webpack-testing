ac643baba87b09b18fe7523933232ca894f3a71f
Manikanta Tankala <im.manikanta@gmail.com>
2019-02-11 00:20:35 +0800

    #23 revert to using mocha and remove mocha-loader

--- test/usersModule.test.js
+++ test/usersModule.test.js
@@ -1,7 +1,8 @@
 const chai = require("chai");
 const assert = chai.assert;
 const expect = chai.expect;
-const usersModule = require("../src/js/usersModule");
+let usersModule = require("../src/js/usersModule");
+const mock = require('mock-require');
 const dummyObject = {
   "id": 1,
   "first_name": "Aradhana",
@@ -11,6 +12,19 @@ const dummyObject = {
   "ip_address": "68.207.239.110",
   "avatar": "https://i.imgur.com/Re7kdwm.jpg"
 };
+const template = `<div class="card">
+<div class="profile-img profile-img__dummy" style="background-image: url('<%= avatar %>');"></div>
+<a href="#">
+  <div class="profile-img profile-img__expand" style="background-image: url('<%= avatar %>');"></div>
+</a>
+<div class="card-body">
+  <h4 class="title"><%= first_name %> <%= last_name %></h4>
+  <p class="email"><%= email %></p>
+</div>
+</div>`;
+
+mock("../templates/card.html", template);
+usersModule = mock.reRequire("../src/js/usersModule");
 
 describe('usersModule', function() {
   it('should expect an object as module value', function() {

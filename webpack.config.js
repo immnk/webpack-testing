a96caa59322f2e6ef758094e8dd438cafa0f70f4
Mani <im.manikanta@gmail.com>
2019-02-12 13:42:03 +0800

    #31 - add optimizations using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ console.log("Webpack is running in " + process.env.NODE_ENV + " mode");
 var path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   mode: debug ? "development" : "production",
@@ -28,10 +30,16 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "css/main.css"
+      filename: debug ? "css/main.css" : "css/main.min.css"
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html"
     }),
   ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+    ],
+  }
 };
